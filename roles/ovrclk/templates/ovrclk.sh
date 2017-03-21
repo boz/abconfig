e58bf892db8e0a821f0a7636288521f96765a328
Adam Bozanich <adam.boz@gmail.com>
2017-03-21 03:07:51 -0700

    ovrclk

---
+++ roles/ovrclk/templates/ovrclk.sh
@@ -0,0 +1,25 @@
+
+export WALKER_PUBLIC_DOMAIN="{{ovrclk.publicdomain}}"
+export WALKER_DEVPOD_HOST="local.$WALKER_PUBLIC_DOMAIN"
+
+export OTS_VERSION="{{ovrclk.ots.version}}"
+export OTS_CHANNEL="{{ovrclk.ots.channel}}"
+
+export WALKER_DEVPOD=1
+
+export OVRCLK_HOME="$GOPATH/src/github.com/ovrclk"
+export WALKER_HOME="$OVRCLK_HOME/walker/_support"
+export OVRCLK_EXAMPLE_HOME="$WALKER_HOME/ovrclk-examples"
+
+export PATH="$WALKER_HOME/bin:$PATH"
+export PATH="$WALKER_HOME/contrib:$PATH"
+export PATH="$OVRCLK_HOME/ots/bin:$PATH"
+export PATH="$OVRCLK_HOME/cli/_support/bin:$PATH"
+
+if [ "$(uname)" != "Darwin" ]; then
+  export WALKER_BOX_DEFAULT_PROVIDER=virtualbox
+fi
+
+alias cdo="cd $OVRCLK_HOME"
+alias cdw="cd $OVRCLK_HOME/walker"
+alias cdwh="cd $WALKER_HOME"
