2739e6ff005cfbc4de63e3709fc15d48651091e7
Adam Bozanich <adam.boz@gmail.com>
2017-09-08 15:22:22 -0700

    cdgo fix, lsof

--- roles/fzf/templates/fzf.zsh
+++ roles/fzf/templates/fzf.zsh
@@ -6,4 +6,4 @@ if [[ -f "{{fzf.dir}}/shell/key-bindings.zsh" ]]; then
   source "{{fzf.dir}}/shell/key-bindings.zsh"
 fi
 
-alias cdgo='cd $(find $GOPATH/src -type d -maxdepth 4  | fzf --with-nth=6,7,8,9 -d/)'
+alias cdgo='cd $(find $GOPATH/src -maxdepth 4 -type d | fzf --with-nth=6,7,8,9 -d/)'
