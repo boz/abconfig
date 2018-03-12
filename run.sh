99991d97f0c94c8c7dbc562fc9008d68466513d1
Adam Bozanich <adam.boz@gmail.com>
2018-03-11 21:59:46 -0700

    small fixes

--- run.sh
+++ run.sh
@@ -6,10 +6,19 @@ else
   hosts="hosts/default"
 fi
 
+tags=""
+
+case "$1" in
+  --tags)
+    tags="$1 $2"
+    shift;shift
+    ;;
+esac
+
 if [ $# -eq 0 ]; then
   args="workstation.yml"
 else
   args=$@
 fi
 
-ansible-playbook -i "$hosts" $args
+ansible-playbook -i "$hosts" $tags $args
